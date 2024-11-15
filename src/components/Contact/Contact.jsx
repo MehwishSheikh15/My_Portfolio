cd4be202d7b5df0e3c1644bcfbb63960956adfb4
unknown <you@example.com>
2024-11-15 23:20:24 +0500

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:mehwishsheikh451sheikh@gmail.com">mehwishsheikh451sheikh@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/mehwish-sheikh-9871442b6">linkedin.com/mehwish-sheikh</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/MehwishSheikh15">github.com/MehwishSheikh15</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};

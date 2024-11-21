37e9ffc2df3f2208dacc7cc5844b822bcfc841c9
Mehwish Sheikh <160774324+MehwishSheikh15@users.noreply.github.com>
2024-11-21 22:16:09 +0500

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -13,20 +11,27 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:mehwishsheikh451sheikh@gmail.com">mehwishsheikh451sheikh@gmail.com</a>
+          <a href="mailto:mehwishsheikh451sheikh@gmail.com">
+            mehwishsheikh451sheikh@gmail.com
+          </a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="www.linkedin.com/in/mehwish-sheikh-9871442b6">linkedin.com/mehwish-sheikh</a>
+          <a href="https://linkedin.com/in/mehwish-sheikh-9871442b6">
+            linkedin.com/mehwish-sheikh
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/MehwishSheikh15">github.com/MehwishSheikh15</a>
+          <a href="https://github.com/MehwishSheikh15">
+            github.com/MehwishSheikh15
+          </a>
         </li>
       </ul>
     </footer>
   );
 };
+ 
